1b0b5cd5873352ebc1ec4a347a69a7841863d6be
吉富　栞汰 <yoshitomikanta@yoshitomishiorinoMacBook-Air.local>
2024-12-17 23:04:49 +0900

    Update app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "717526d0b4631162d0c28bb33b2e1089",
+const RESOURCES = {"flutter_bootstrap.js": "e270f1baa88442278d429020c7cfa93b",
 "version.json": "a1f3fb16d9290b637dc368abe87ef2e4",
 "index.html": "a3cbaca46164655787de42ebe1dff542",
 "/": "a3cbaca46164655787de42ebe1dff542",
